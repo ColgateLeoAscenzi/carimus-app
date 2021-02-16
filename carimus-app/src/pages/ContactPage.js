7285578b5c59da691ad239d0916f006aae2f65da
Leonardo Ascenzi <lascenzi@colgate.edu>
2021-02-16 17:36:38 -0500

    partially finished the contact page

--- carimus-app/src/pages/ContactPage.js
+++ carimus-app/src/pages/ContactPage.js
@@ -2,11 +2,132 @@ import "./../App.css";
 import Footer from "../components/Footer/Footer";
 import BlankComponent from "./../components/BlankComponent";
 
+const firstSection = {
+  width: "100vw",
+  height: "90vh",
+  display: "flex",
+  flexDirection: "column",
+  paddingLeft: "4vw",
+  paddingTop: "10vh",
+};
+
+const upperStyle = {
+  width: "100vw",
+  height: "10vh",
+  fontWeight: "bolder",
+  letterSpacing: "3.3px",
+};
+
+const lowerStyle = { display: "flex", flexDirection: "row" };
+
+const innerLeft = { display: "flex", flexDirection: "column", width: "49vw" };
+const innerRight = { display: "flex", flexDirection: "row", width: "49vw" };
+
 function ContactPage() {
   return (
     <div className="ContactPage">
-      <BlankComponent />
-      Contact Page
+      <div style={firstSection}>
+        <div style={upperStyle}>CONTACT</div>
+        <div style={lowerStyle}>
+          <div style={innerLeft}>Ready for liftoff? Let's launch.</div>
+          <div style={innerRight}>
+            <div
+              style={{ display: "flex", flexDirection: "column", width: "50%" }}
+            >
+              <span
+                style={{
+                  fontWeight: "bolder",
+                  letterSpacing: "3.3px",
+                  paddingBottom: "2vh",
+                }}
+              >
+                LOCATION
+              </span>
+              <span
+                style={{
+                  fontWeight: "bolder",
+                  letterSpacing: "3.3px",
+                  color: "#78e690",
+                  fontSize: "2em",
+                }}
+              >
+                Raleigh, NC
+              </span>
+              <span
+                style={{
+                  fontWeight: "bolder",
+                  letterSpacing: "3.3px",
+                }}
+              >
+                120 Saint Albans Drive, Apt 237
+              </span>
+              <span
+                style={{
+                  fontWeight: "bolder",
+                  letterSpacing: "3.3px",
+                }}
+              >
+                Raleigh, NC 27609
+              </span>
+              <div
+                onClick={() =>
+                  (window.location.href =
+                    "https://goo.gl/maps/ZGXCci5V4z4pUXvg9")
+                }
+              >
+                <img
+                  style={{ height: "1.5vw", paddingRight: "1vw" }}
+                  src="/images/rocketInverse.png"
+                  alt="rocket"
+                />
+                <a
+                  stlye={{ fontWeight: "bolder", letterSpacing: "3.3px" }}
+                  href="https://goo.gl/maps/ZGXCci5V4z4pUXvg9"
+                >
+                  DIRECTIONS
+                </a>
+              </div>
+            </div>
+            <div
+              style={{ display: "flex", flexDirection: "column", width: "50%" }}
+            >
+              <span
+                style={{
+                  fontWeight: "bolder",
+                  letterSpacing: "3.3px",
+                  paddingBottom: "2vh",
+                }}
+              >
+                INFO
+              </span>
+              <span
+                style={{
+                  fontWeight: "bolder",
+                  letterSpacing: "3.3px",
+                }}
+              >
+                Phone 919 710 5002
+              </span>
+              <span
+                style={{
+                  fontWeight: "bolder",
+                  letterSpacing: "3.3px",
+                }}
+              >
+                Personal Email leo.ascenzi@gmail.com
+              </span>
+              <span
+                style={{
+                  fontWeight: "bolder",
+                  letterSpacing: "3.3px",
+                }}
+              >
+                Work Email lascenzi@prometheusgroup.com
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
--- carimus-app/src/pages/HomePage.js
+++ carimus-app/src/pages/HomePage.js
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 const firstSection = {
   width: "100vw",
-  height: "90vh",
+  height: "68vh",
 };
 
 const blurbWrapper = {
@@ -32,6 +32,30 @@ const fillerText = {
   fontWeigh: "bolder",
 };
 
+const secondSection = {
+  height: "85vh",
+  width: "100vw",
+  backgroundColor: "#1b1650",
+  display: "flex",
+  flexDirection: "column",
+};
+
+const buttonStyle = {
+  marginTop: "4vh",
+  backgroundImage: "linear-gradient(to right, #d25528, #da1d67)",
+  border: "none",
+  outline: "none",
+  color: "white",
+  paddingTop: "0.5vw",
+  paddingBottom: "0.5vw",
+  paddingLeft: "3vw",
+  paddingRight: "8vw",
+  fontFamily: "Montserrat",
+  fontWeight: "bolder",
+  letterSpacing: "3.3px",
+  fontSize: "0.8em",
+};
+
 function HomePage() {
   return (
     <>
@@ -46,9 +70,9 @@ function HomePage() {
               }}
               transition={{ y: { duration: 1 } }}
             >
-              A <span style={{ color: "#78e690" }}>world-class team</span> to
-              build your brand, strategy, and technology, taking your business
-              to the next level.
+              A <span style={{ color: "#78e690" }}>world-class developer</span>{" "}
+              to build your brand, strategy, and technology, taking Carimus to
+              the next level.
             </motion.div>
             <motion.div
               style={fillerText}
@@ -61,16 +85,64 @@ function HomePage() {
                 opacity: { duration: 1, delay: 0.8 },
               }}
             >
-              We are a diverse team of strategists, creatives and engineers
-              building the best brands, software and technology solutions
-              available. Together, we do important work with people and
-              companies we admire.
+              I am a motivated, hardworking software engineer who would love to
+              work alongside, creatives and engineers to build the best brands,
+              software and technology solutions available. Together, I want to
+              do important work with people and companies we both admire.
             </motion.div>
           </div>
 
           <div style={carouselWrapper}>
             <PicCarousel />
           </div>
+        </div>
+        <div style={secondSection}>
+          <div style={{ width: "100vw", height: "36%", padding: "2vw" }}>
+            <a href="./about">
+              <button style={buttonStyle}>ABOUT US</button>
+            </a>
+            <br />
+            <div
+              style={{ paddingTop: "6vh", cursor: "pointer" }}
+              onClick={() => (window.location.href = "./contact")}
+            >
+              <motion.img
+                src="/images/rocket.png"
+                whileHover={{ y: -10 }}
+              ></motion.img>
+              <span
+                style={{
+                  paddingLeft: "4px",
+                  fontWeight: "bolder",
+                  letterSpacing: "3.6px",
+                }}
+              >
+                LET'S LAUNCH
+              </span>
+            </div>
+          </div>
+          <div
+            style={{
+              width: "100vw",
+              height: "32%",
+              justifyContent: "center",
+              display: "flex",
+              textAlign: "center",
+            }}
+          >
+            <div style={{ width: "33vw" }}>
+              <span
+                style={{
+                  fontWeight: "bolder",
+                  fontSize: "1.3em",
+                }}
+              >
+                Delivering strategy, creative and technical services for the
+                world's leading brands.
+              </span>
+            </div>
+          </div>
+          <div style={{ width: "100vw", height: "32%" }}> like these</div>
         </div>
         <Footer />
       </div>
